96c38d9fb5c546b867326919d8d63ff740a6b6e5
Park Hyun Seo <hyeonseo9669@hanmail.net>
2018-02-13 14:03:03 +0000

    Add ChatBoard, ChatWrapper, User, UserList

--- front/src/containers/App.js
+++ front/src/containers/App.js
@@ -1,77 +1,55 @@
 import React, { Component } from 'react';
 import {
-  Header
+  Header,
+  UserList
+} from './components'
+import {
+  ChatContainer,
 } from './components'
 import './App.css';
 
-import { SocketProvider } from 'socket.io-react';
-import { socketConnect } from 'socket.io-react'
-import io from 'socket.io';
-
 class App extends Component {
-  
-  socket =  io.connect();
-  
-  state = {
-    messages: [ ],
-    users: [ ],
-    name: '',
-    text: '',
-  };
-  
+ 
   constructor(props){
-    super(props);
-      
-    this.socket.on('connect', () => {
-      this.setName();
-    });
-  
-    this.socket.on('message', function (msg) {
-      const { messages } = this.state
-      
-      this.setState({
-        messages : messages.concat(msg)
-      })
-
-    });
+    super();
     
-    this.socket.on('message', function (msg) {
-      const { messages } = this.state
-      this.setState({
-        messages : messages.concat(msg)
-      })
-    });
+    const { socket } = this.props;
     
-    this.socket.on('user', function (names) {
-      this.setState({
-        users: names
-      });
-      // 수정
-      $('#localusers').append($('<li class="collection-item">' + names + '</li>'))
-    });
-  
+    this.state = {  
+      messages: [ ],
+      users: [ ],
+      name: '',
+      text: '',
+      socket: socket
+    }
   }
 
+  componentDidMount(){
+    
+  }
+  
   send = () => {
-    const { text } = this.state
+    const { text, socket } = this.state
     
     console.log('Sending message:', text);
     
-    this.socket.emit('message', text);
-    this.text = '';
+    socket.emit('message', text);
+    text = '';
   };
   
   setName = () => {
-    const { name } = this.state
-    this.socket.emit('identify', name);
+    const { name, socket } = this.state
+    
+    socket.emit('identify', name);
   };
 
   render() {
+    const { messages, users, name, text, socket } = this.state;
+    
     return (
-      <div className="App">
-      <SocketProvider socket={this.socket}>
-      
-      </SocketProvider>
+      <div>
+        <Head/>
+        <ChatContainer socket={socket}/>
       </div>
     );
   }
